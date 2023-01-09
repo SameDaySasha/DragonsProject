64a9a80e87dc1dc76143357f033a8f6972e3b48a
Christine <clong9712@gmail.com>
2023-01-08 22:58:16 -0500

    finished

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,18 +1,18 @@
 const Dragon = require("./dragon.js");
 
-class EvilDragon extends Dragon{
-    constructor(name, color, evilDoings, nemesis){
-        super (name, color);
-        this.evilDoings = evilDoings;
-        this.nemesis = nemesis;
-    }
-    dontInviteThemOverForDinner(){
-        this.evilDoings.forEach(action => {
-            console.log(`${this.name} will ${action}`)
-        });
-    }
-    burnsNemesis(){
-        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
-    }
+class EvilDragon extends Dragon {
+  constructor(name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+  dontInviteThemOverForDinner() {
+    this.evilDoings.forEach((action) => {
+      console.log(`${this.name} will ${action}`);
+    });
+  }
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
 }
 module.exports = EvilDragon;
