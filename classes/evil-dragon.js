2716e40426463dad36df0933231f149816ee951b
SameDaySasha <111303031+SameDaySasha@users.noreply.github.com>
2023-01-09 17:51:11 -0500

    Evil Dragon p1

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,11 @@
-// Your code here
+const Dragon = require("./dragon.js");
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis){
+        super(name, color)
+        this.evilDoings = evilDoings
+        this.nemsis = nemesis
+    }
+}
+
+module.exports = EvilDragon;
